8ad766660e9c2795db10aba58291d0c5df1f504e
haimin <med53@mail.ru>
2018-09-16 11:49:20 +0300

    os added

--- converter.py
+++ converter.py
@@ -1,16 +1,18 @@
 import pydicom as pyd
+import os
 
 def jp2_to_dcm(jp2, dcm):
         # convert from jp2 to dcm file
         # not decompressed
+        # libgdcm-tools have to be installed
 
-      cmd_string = 'gdcmimg ' + jp2 + ' ' + dcm
-      files.os.system(cmd_string)
+      cmd_string = 'gdcmimg ' + jp2 + ' ' + dcm 
+      os.system(cmd_string)
       
-def get_total_files_number(folder):
+def get_total_files_number(folder): # helper function
         
         n=0
-        for root, dirs, images in files.os.walk(folder):
+        for root, dirs, images in os.walk(folder):
           
           #print('total files: ', len(images))
           for img in images:
@@ -24,21 +26,21 @@ def convert_dir(convert_dir):
         file_num = get_total_files_number(convert_dir)
         # convert and decompress jp2 images in given folder
         n = 1
-        for root, dirs, images in files.os.walk(convert_dir):
+        for root, dirs, images in os.walk(convert_dir):
 
             for img in images:
                 if img.endswith('.jp2'):
                     img_name = img.split('/')[-1][:-8]
                     #print(img_name)
-                    if img_name not in files.os.listdir(root):
-                      ds2 = pyd.dcmread(files.os.path.join(root, 'DICOM', img_name + '.dcm'), force=True)
+                    if img_name not in os.listdir(root):
+                      ds2 = pyd.dcmread(os.path.join(root, 'DICOM', img_name + '.dcm'), force=True)
                       if ds2.ImageType[0] != 'ORIGINAL':
                         try:
-                          jp2_to_dcm(files.os.path.join(root, img), files.os.path.join(root, img_name))
-                          ds = pyd.dcmread(files.os.path.join(root, img_name))
+                          jp2_to_dcm(os.path.join(root, img), os.path.join(root, img_name))
+                          ds = pyd.dcmread(os.path.join(root, img_name))
                           ds.decompress()
                           ds.Modality, ds.ImageType = ds2.Modality, ds2.ImageType
-                          ds.save_as(files.os.path.join(root, img_name))
+                          ds.save_as(os.path.join(root, img_name))
                           n +=1
                           print('converted {}, {} from {}'.format(img, n, file_num))
                         except ValueError:
